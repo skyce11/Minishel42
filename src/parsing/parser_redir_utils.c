d84de8c1c3ae7e6ced61209093ca0cab113e6763
Amelie Pocchiolo <ampocchi@c2r17s1.42madrid.com>
2025-05-13 18:06:58 +0200

    test

--- src/parsing/parser_redir_utils.c
+++ src/parsing/parser_redir_utils.c
@@ -3,14 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   parser_redir_utils.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: migonzal <migonzal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ampocchi <ampocchi@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 13:09:02 by migonzal          #+#    #+#             */
-/*   Updated: 2025/05/10 15:17:07 by migonzal         ###   ########.fr       */
+/*   Updated: 2025/05/13 17:52:54 by ampocchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// static void	locate_redir_aux(char *redir, int *i, int *type, char *quote)
+// {
+// 	if (redir[*i] == '"' || redir[*i] == '\'')
+// 		*quote = redir[*i];
+// 	else
+// 	{
+// 		if (redir[*i] == '>' && redir[*i + 1] && redir[*i + 1] == '>')
+// 		{
+// 			*i = *i + 1;
+// 			*type = 3;
+// 		}
+// 		else if (redir[*i] == '>')
+// 			*type = 1;
+// 		else if (redir[*i] == '<'
+// 			&& redir[*i + 1] && redir[*i + 1] == '<')
+// 		{
+// 			*i = *i + 1;
+// 			*type = 2;
+// 		}
+// 		else if (redir[*i] == '<')
+// 			*type = 0;
+// 	}
+// }
 
 /// @brief Identifies the type of redirection operator in a string.
 /// Scans the input while handling quoted sections to correctly detect
@@ -34,6 +58,7 @@ static int	locate_redir(char *redir, int *i)
 		}
 		else
 		{
+			// locate_redir_aux(redir, i, &type, &quote);
 			if (redir[*i] == '"' || redir[*i] == '\'')
 				quote = redir[*i];
 			else
@@ -60,6 +85,45 @@ static int	locate_redir(char *redir, int *i)
 	return (type);
 }
 
+// static int	process_unquoted_char(char *redir, int *i, char *quote)
+// {
+// 	if (redir[*i] == '"' || redir[*i] == '\'')
+// 		*quote = redir[*i];
+// 	else if (redir[*i] == ' ' || redir[*i] == '\t' ||
+// 			redir[*i] == '\n' || redir[*i] == '<' || redir[*i] == '>')
+// 		return (0);
+// 	return (1);
+// }
+
+// static size_t	extract_redir_word(char *redir, int *i)
+// {
+// 	char	quote;
+// 	size_t	length;
+
+// 	quote = 0;
+// 	length = 0;
+// 	while (redir[*i])
+// 	{
+// 		if (quote == '"' || quote == '\'')
+// 		{
+// 			if (redir[*i] == quote)
+// 			{
+// 				(*i)++;
+// 				length++;
+// 				break ;
+// 			}
+// 		}
+// 		else
+// 		{
+// 			if (!process_unquoted_char(redir, i, &quote))
+// 				break;
+// 		}
+// 		length++;
+// 		(*i)++;
+// 	}
+// 	return (length);
+// }
+
 static size_t	preprocess_redir_file(char *redir, int *i,
 			int *status, char **word_start)
 {
@@ -78,6 +142,7 @@ static size_t	preprocess_redir_file(char *redir, int *i,
 		return (0);
 	}
 	*word_start = redir + *i;
+		// length = extract_redir_word(redir, i);
 	while (redir[*i] && *word_start)
 	{
 		if (quote == '"' || quote == '\'')
