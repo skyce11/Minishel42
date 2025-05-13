022bc73bd48ea1e366c768b44653cf485b005800
Miguel angel Gonzalez gonzalez <migonzal@c2r6s6.42madrid.com>
2025-05-13 13:55:12 +0200

    Quotes in process

--- src/expansor/expansor.c
+++ src/expansor/expansor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expansor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ampocchi <ampocchi@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: migonzal <migonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 10:21:27 by migonzal          #+#    #+#             */
-/*   Updated: 2025/05/11 17:16:38 by ampocchi         ###   ########.fr       */
+/*   Updated: 2025/05/13 13:20:32 by migonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ char	*expansor(t_tools *tools)
 	tools->arg_str = expand_exit_status(tools->arg_str, tools->exit_status);
 	idx = dollar_after(tools->arg_str);
 	if (idx >= 0
-		&& (idx == 0) //|| tools->arg_str[idx - 2] != '\'')
+		&& ((idx == 0) || tools->arg_str[idx - 2] != '\'')
 		&& tools->arg_str[idx] != '\0')
 	{
 		aux = detect_dollar(tools);
@@ -90,54 +90,154 @@ char	*expansor(t_tools *tools)
 	return (tools->arg_str);
 }
 
-static char	*process_dollar(t_tools *tools, char *aux, int *i)
+// static char	*process_dollar(t_tools *tools, char *aux, int *i)
+// {
+// 	char	*replace_str;
+// 	char	*tmp;
+
+// 	replace_str = NULL;
+// 	tmp = NULL;
+// 	if (tools->arg_str[*i] == '$' && tools->arg_str[*i + 1] == '?')
+// 	{
+// 		replace_str = ft_itoa(tools->exit_status);
+// 		*i += 2;
+// 	}
+// 	else if (tools->arg_str[*i] == '$' && (tools->arg_str[*i + 1] != ' '
+// 			&& (tools->arg_str[*i + 1] != '"'
+// 				|| tools->arg_str[*i + 2] != '\0'))
+// 		&& tools->arg_str[*i + 1] != '\0')
+// 	{
+// 		*i += loop_dollar(tools, &aux, *i);
+// 		return (aux);
+// 	}
+// 	else
+// 		replace_str = char_to_str(tools->arg_str[(*i)++]);
+// 	tmp = ft_strjoin(aux, replace_str);
+// 	free(replace_str);
+// 	free(aux);
+// 	aux = ft_strdup(tmp);
+// 	return (free(tmp), aux);
+// }
+
+// /// @brief Transforma un comando del usuario que contiene variables en una
+// /// versión interpretada antes de ser ejecutada.
+// /// @param tools Estructura con las herramientas y datos necesarios para
+// /// la gestión del comando.
+// /// @return La cadena modificada, que contiene el comando con las variables
+// /// expandidas.
+// char	*detect_dollar(t_tools *tools)
+// {
+// 	int		i;
+// 	char	*aux;
+
+// 	i = 0;
+// 	aux = ft_strdup("\0");
+// 	while (tools->arg_str[i])
+// 	{
+// 		i += digit_after_dollar(i, tools->arg_str);
+// 		aux = process_dollar(tools, aux, &i);
+// 		if (!aux)
+// 			return (NULL);
+// 	}
+// 	return (aux);
+// }
+
+static char	*process_dollar(t_tools *tools, char *aux, int *i, int in_squote, int in_dquote)
 {
 	char	*replace_str;
 	char	*tmp;
 
 	replace_str = NULL;
 	tmp = NULL;
-	if (tools->arg_str[*i] == '$' && tools->arg_str[*i + 1] == '?')
+
+	if (in_squote) // Estamos dentro de comillas simples
 	{
-		replace_str = ft_itoa(tools->exit_status);
-		*i += 2;
+		replace_str = char_to_str(tools->arg_str[(*i)++]);
 	}
-	else if (tools->arg_str[*i] == '$' && (tools->arg_str[*i + 1] != ' '
-			&& (tools->arg_str[*i + 1] != '"'
-				|| tools->arg_str[*i + 2] != '\0'))
-		&& tools->arg_str[*i + 1] != '\0')
+	else if (in_dquote && tools->arg_str[*i] == '$') // Comillas dobles permiten $
 	{
-		*i += loop_dollar(tools, &aux, *i);
-		return (aux);
+		if (tools->arg_str[*i + 1] == '?') // Manejo de $? dentro de comillas dobles
+		{
+			replace_str = ft_itoa(tools->exit_status);
+			*i += 2;
+		}
+		else
+		{
+			*i += loop_dollar(tools, &aux, *i);
+			return (aux);
+		}
+	}
+	else if (tools->arg_str[*i] == '$') // Sin comillas o fuera de comillas dobles
+	{
+		if (tools->arg_str[*i + 1] == '?')
+		{
+			replace_str = ft_itoa(tools->exit_status);
+			*i += 2;
+		}
+		else
+		{
+			*i += loop_dollar(tools, &aux, *i);
+			return (aux);
+		}
 	}
 	else
+	{
 		replace_str = char_to_str(tools->arg_str[(*i)++]);
+	}
+
 	tmp = ft_strjoin(aux, replace_str);
 	free(replace_str);
 	free(aux);
 	aux = ft_strdup(tmp);
-	return (free(tmp), aux);
+	free(tmp);
+	return (aux);
 }
 
-/// @brief Transforma un comando del usuario que contiene variables en una
-/// versión interpretada antes de ser ejecutada.
-/// @param tools Estructura con las herramientas y datos necesarios para
-/// la gestión del comando.
-/// @return La cadena modificada, que contiene el comando con las variables
-/// expandidas.
+/**
+ * @brief Transforma un comando del usuario que contiene variables en una versión interpretada.
+ * @param tools Estructura con las herramientas y datos necesarios.
+ * @return La cadena modificada con las variables expandidas.
+ */
 char	*detect_dollar(t_tools *tools)
 {
 	int		i;
+	int		in_squote;
+	int		in_dquote;
 	char	*aux;
 
 	i = 0;
+	in_squote = 0;
+	in_dquote = 0;
+	aux = NULL;
+
 	aux = ft_strdup("\0");
+
 	while (tools->arg_str[i])
 	{
-		i += digit_after_dollar(i, tools->arg_str);
-		aux = process_dollar(tools, aux, &i);
+		// Actualizamos el estado de las comillas
+		if (tools->arg_str[i] == '"' && !in_squote)
+		{
+			in_dquote = !in_dquote;
+		}
+		else if (tools->arg_str[i] == '\'' && !in_dquote)
+		{
+			in_squote = !in_squote;
+		}
+
+		// Procesamos la expansión del dólar
+		if (tools->arg_str[i] == '$' && !in_squote)
+		{
+			aux = process_dollar(tools, aux, &i, in_squote, in_dquote);
+		}
+		else
+		{
+			aux = process_dollar(tools, aux, &i, in_squote, in_dquote);
+		}
+
 		if (!aux)
+		{
 			return (NULL);
+		}
 	}
 	return (aux);
 }
